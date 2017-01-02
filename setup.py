70e755bcf944fee807bbcc8f97e33d925c9e4e7e
Simon Engledew <simon.engledew@osirium.com>
2017-01-02 00:05:44 +0000

    fix setup.py

--- setup.py
+++ setup.py
@@ -2,22 +2,24 @@ from setuptools import setup, find_packages
 
 setup(
     name='er',
-    version='0.1',
+    version='0.2',
 
     description='Generate data that matches a given regular expression.',
     author="Simon Engledew",
     author_email="simon@engledew.com",
     url="https://github.com/simon-engledew/python-er",
 
-    install_requires = [
+    install_requires=[
     ],
-    extras_require = {
-      'tests': [
-        'nose'
-      ],
-      ':python_version < "3.4"': ["enum34"]
+    extras_require={
+        'tests': [
+            'nose'
+        ],
+        ':python_version < "3.4"': [
+            "enum34"
+        ]
     },
-    entry_points = {
+    entry_points={
         'console_scripts': [
             'er = er:main',
         ],
@@ -26,7 +28,7 @@ setup(
     include_package_data=False,
     packages=find_packages(),
     license='MIT',
-    keywords = [
+    keywords=[
         're',
         'test',
         'testing',
@@ -34,9 +36,18 @@ setup(
         'fake',
         'regular expression'
     ],
-    classifiers = [
+    classifiers=[
         'Development Status :: 1 - Planning',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.0',
+        'Programming Language :: Python :: 3.1',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6'
     ],
 )
